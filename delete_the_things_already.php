64f612f763530d1e95146f4a3c7c79c29ba92e09
FreshyMichael <50490309+FreshyMichael@users.noreply.github.com>
2021-09-10 10:25:42 -0400

    Update delete_the_things_already.php

--- delete_the_things_already.php
+++ delete_the_things_already.php
@@ -1,20 +1,4 @@
 <?php
-/**
-* Plugin Name: WooCommerce AutoDelete Uploads
-* Plugin URI: https://github.com/FreshyMichael/Plugin-Starter
-* Description: Automatically delete files uploaded to the WooCommerce Uploads folder, if older than 7 days. 
-* Version: 1.0.0
-* Author: FreshySites
-* Author URI: https://freshysites.com/
-* License: GNU v3.0
-*/
-
-if ( ! defined( 'ABSPATH' ) ) {
-	die();
-}
-
-/* PluginName Start */
-//______________________________________________________________________________
 
 // lets get the directory file contents
   include "wp-load.php";
@@ -75,32 +59,3 @@ if ( ! defined( 'ABSPATH' ) ) {
 		echo $filename . " " . " File Extension: " . $extension . "<br>";
 	}
      }
-
-
-//______________________________________________________________________________
-// All About Updates
-
-//  Begin Version Control | Auto Update Checker
-require 'plugin-update-checker/plugin-update-checker.php';
-$myUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
-// ***IMPORTANT*** Update this path to New Github Repository Master Branch Path
-	'https://github.com/FreshyMichael/woocommerce_autodelete_uploads',
-	__FILE__,
-// ***IMPORTANT*** Update this to New Repository Master Branch Path
-	'woocommerce_autodelete_uploads'
-);
-//Enable Releases
-$myUpdateChecker->getVcsApi()->enableReleaseAssets();
-//Optional: If you're using a private repository, specify the access token like this:
-//
-//
-//Future Update Note: Comment in these sections and add token and branch information once private git established
-//
-//
-//$myUpdateChecker->setAuthentication('your-token-here');
-//Optional: Set the branch that contains the stable release.
-//$myUpdateChecker->setBranch('stable-branch-name');
-
-//______________________________________________________________________________
-/* PluginName End */
-?>
